175ba1f0eb61341812fa81cfccc2d3a8facc5ca9
jose-016al <jose_016al@outlook.com>
2022-05-09 21:14:21 +0200

    Changes script

--- examen.sql
+++ examen.sql
@@ -15,6 +15,10 @@ SELECT CONCAT(nombre, " ", apellido1, " ", IFNULL(apellido2, " ")) AS nombre
 FROM ponente ORDER BY apellido1 LIMIT 0, 6;
 
 -- Ejercciio 4
+SELECT idAsistente AS identificador, nombre, CONCAT(apellido1, " ", IFNULL(apellido2, " ")) 
+AS apellidos  FROM asistente UNION SELECT idPonente AS identificador, nombre, 
+CONCAT(apellido1, " ", IFNULL(apellido2, " ")) AS apellidos FROM ponente
+ORDER BY apellidos, nombre;
 
 -- Ejercicio 5
 SELECT C.tema, ROUND(C.precio * 10.5) AS precio, DATE_FORMAT(fecha, "%d-%m-$Y") AS fecha,
@@ -22,13 +26,21 @@ S.nombreSala, S.capacidad FROM conferencia AS C JOIN sala AS S ON (C.nombreSala 
 WHERE C.turno LIKE "T" ORDER BY fecha;
 
 -- Ejercicio 6
-SELECT A.nombre, SUM(C.precio) AS precio
+SELECT A.nombre, A.apellido1, A.apellido2, SUM(C.precio) AS precio
 FROM asistente AS A JOIN asiste AS asi ON (A.idAsistente = asi.idAsistente)
 JOIN conferencia AS C ON (asi.idConferencia = C.idConferencia)
-GROUP BY nombre HAVING precio >= 45 ORDER BY precio DESC;
+GROUP BY A.nombre, A.apellido1, A.apellido2 HAVING precio >= 45 ORDER BY precio DESC;
 
 -- Ejercicio 7
+SELECT DISTINCT S.nombreSala, CONCAT(P.apellido1, " ", IFNULL(P.apellido2, " "), P.nombre) 
+AS nombre FROM sala AS S LEFT JOIN conferencia AS C ON (S.nombreSala = C.nombreSala)
+LEFT JOIN participa AS par ON (C.idConferencia = par.idConferencia)
+LEFT JOIN ponente AS P ON (par.idPonente = P.idPonente) 
+ORDER BY S.nombreSala, nombre ASC;
 
+-- eJERCICIO 8
+SELECT tema FROM conferencia WHERE turno LIKE "T" AND precio < (SELECT MIN(precio) 
+FROM conferencia WHERE turno LIKE "M") ORDER BY tema ASC; 
 
 
 
